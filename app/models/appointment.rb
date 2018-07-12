13a0544ad76500e08d25a0de81f74d126232fe05
Zac Willmington <zac.willmington@gmail.com>
2018-07-12 17:40:07 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,8 +2,8 @@ class Appointment < ActiveRecord::Base
     belongs_to :doctor
     belongs_to :patient
 
-    def parse_appointment_datetime
-        Date.parse(self.appointment_datetime).strftime("%B %d, %Y at 20:20")
-    end
+    # def parse_appointment_datetime
+    #     Date.parse(self.appointment_datetime).strftime("%B %d, %Y at 20:20")
+    # end
 
 end
